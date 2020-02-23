821cd6c6b2c6b375b62fa7395a02f4e35d412405
hemalathamallavaram <hemalathamallavaram.cgg@gmail.com>
2020-02-23 12:17:42 +0000

    Updates

---
+++ precache-manifest.09d94252a40ae58373f86b219f231ca9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40406694ef04298bac207a25960ab5fc",
+    "url": "/random-name-game-react/index.html"
+  },
+  {
+    "revision": "b52e7401a6d2a3ffdd0b",
+    "url": "/random-name-game-react/static/css/2.c68d7c85.chunk.css"
+  },
+  {
+    "revision": "b767e6b56b3d0a4b1bce",
+    "url": "/random-name-game-react/static/css/main.84501f83.chunk.css"
+  },
+  {
+    "revision": "b52e7401a6d2a3ffdd0b",
+    "url": "/random-name-game-react/static/js/2.38e702d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-name-game-react/static/js/2.38e702d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b767e6b56b3d0a4b1bce",
+    "url": "/random-name-game-react/static/js/main.13436189.chunk.js"
+  },
+  {
+    "revision": "26a7c623f3bfe13feabc",
+    "url": "/random-name-game-react/static/js/runtime-main.fa499408.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/random-name-game-react/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/random-name-game-react/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/random-name-game-react/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/random-name-game-react/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/random-name-game-react/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
